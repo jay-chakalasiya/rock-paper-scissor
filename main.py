f0af80c8677bf8aa5b113e018d64a5f8ff7d3823
Jay Chakalasiya <14358488+jay-chakalasiya@users.noreply.github.com>
2021-11-04 22:00:48 -0700

    Added playtime instructions

--- main.py
+++ main.py
@@ -18,6 +18,16 @@ def main():
     if args.input_data:
         train(agent, args.input_data)
 
+    for i in range(5):
+        print('')
+    print('==========> Welcome to Rock-Paper-Scissor <===========')
+    print(' When prompted the input , you can choose R, P, S : chatcters for Rock, Paper or Scissor(It is case sensitive)')
+    print('')
+    print(' Other input option is F and then there will be prompt to provice input va a file, the file must contain R, P or S, each action in new line, example file is provided as input.txt')
+    print('')
+    print(' Input E to exit the game')
+    print('')
+
     Game = RockPaperScissor(agent)
     Game.play_single_interactive_round()
 
